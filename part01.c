ad1560d728fb74fb48a4cf6f500f52d039e5dad0
AbdullrahmanHMD <manofsteel272@yahoo.com>
2021-05-01 23:51:28 +0300

    Add constants and initialized threads. Add condition variable and mutex lock

--- part01.c
+++ part01.c
@@ -5,14 +5,21 @@
 #include <ctype.h>
 #include <pthread.h>
 
-pthread_t tid[1024] = {0};
+#define MAX_THREADS 1000
+
+pthread_t threads[MAX_THREADS] = {0};
 int thread_count = 0;
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t condition_variable = PTHREAD_COND_INITIALIZER;
+
 void createThread(void *(func)(void* vargp));
+void *sayHello();
+void doStuff(int N);
 
 int main(int argc, char *argv[]){
 
-  // REMINDER: This code should be run as follows:
+  // NOTE: This code should be run as follows:
   //                   gcc -pthread -o [file name].c [output name]
   //           Or, alternatively:
   //                   gcc -o [output name] [file name].c -pthread
@@ -23,10 +30,35 @@ int main(int argc, char *argv[]){
   char* questionProbability = argv[4];
   char* numberOfQuestions = argv[6];  
 
+  int N = (*numberOfThreads) -'0';
+
+  printf("%d\n", N);
+  
+  doStuff(N);
+
+  // Wait for the threads to complete.
+  for (int i = 0; i < N; i++)
+    pthread_join(threads[i], NULL);
+  
+  printf("%d\n", thread_count);
   
   return 0;
 }
 
+void doStuff(int N){
+  for(int i = 0; i < N; i++){
+    createThread(sayHello);
+  }
+}
+
+void *sayHello(void *id){
+  pthread_mutex_lock(&mutex);
+  pthread_cond_wait(&condition_variable, &mutex);
+  printf("Hello\n");
+  pthread_mutex_unlock(&mutex);
+  pthread_cond_signal(&condition_variable);
+}
+
 void createThread(void *(func)(void* vargp)){
-   pthread_create(&tid[thread_count++], NULL, func, NULL);
+  pthread_create(&threads[thread_count++], NULL, func, NULL);
 }
